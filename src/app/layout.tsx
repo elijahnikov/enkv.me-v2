51e6b8a07e95e4bbd3cd7d40d6a5bbbcf8d3320d
elijahnikov <enkv@proton.me>
2024-08-01 21:53:25 +0100

    first push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+import { cn } from "@/lib/utils";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(inter.className, GeistSans.variable, GeistMono.variable)}
+      >
+        {children}
+      </body>
     </html>
   );
 }
